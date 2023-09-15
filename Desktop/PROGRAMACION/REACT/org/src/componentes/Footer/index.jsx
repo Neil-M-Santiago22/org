5ab26d8a10a5e7e9e84912a17065363edd50f69b
Neil <neil.maldonado@gmail.com>
2023-09-14 21:56:48 -0500

    curso completado

---
+++ Desktop/PROGRAMACION/REACT/org/src/componentes/Footer/index.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return <footer className="footer" style={{backgroundImage: "url(/img/Footer.png"}}>
+    <div className="redes">
+        <a href="https//www.aluracursos.com/"> 
+            <img src="/img/facebook.png" alt="Facebook" target="_blank" />
+        </a>
+        <a href="https//www.aluracursos.com/"> 
+            <img src="/img/twitter.png" alt="Twitter"  target="_blank"/>
+        </a>
+        <a href="https//www.aluracursos.com/"> 
+            <img src="/img/instagram.png" alt="Instagram" target="_blank" />
+        </a>
+    </div>
+    <strong> Desarrolado por Neil Maldonado</strong>
+    </footer>
+}
+export default Footer
